f35cb9784cc37da441ed1435f4bcbb6a771c4fa5
Ailzr <woderegister@pm.me>
2023-06-08 18:05:31 +0800

    2nd version

---
+++ temp.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    https://www.ailzr.com
+// @version      0.1
+// @description  new script
+// @author       Ailzr
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(
+    function() {
+        'use strict';
+
+        console.log('Hello World!');
+    }
+)();
