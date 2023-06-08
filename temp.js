c1463f572af54b96902dc478c16d9ac93f139cbc
Ailzr <woderegister@pm.me>
2023-06-08 18:43:39 +0800

    add license

--- temp.js
+++ temp.js
@@ -4,6 +4,7 @@
 // @version      0.2
 // @description  new script
 // @author       Ailzr
+// @license      AGPL
 // @match        *://*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
